d1beab1deb8467749c38e9e0e84befef88ff6f7a
sree-bh <sritama.imps@gmail.com>
2015-11-24 15:27:18 +0530

    added code for routing

--- react-snap/main.js
+++ react-snap/main.js
@@ -1,11 +1,18 @@
 'use strict';
 
-var React = require('react'),
+
+var ReactRouter = require('react-router'),
+    Router = ReactRouter.Router,
+    Route = ReactRouter.Route,
+    Link = ReactRouter.Link,
+    History = ReactRouter.History,
+    IndexRoute = ReactRouter.IndexRoute,
+    history = require('history'),
+    React = require('react'),
     ReactDOM = require('react-dom'),
     lastSnap,
-    ssInterval;
-
-var basePath = 'resources/gallery/',
+    ssInterval,
+    basePath = 'resources/gallery/',
     snapList = [
         {
         	path: basePath + 'Koala.jpg',
@@ -33,27 +40,43 @@ var basePath = 'resources/gallery/',
         }
     ];
 
+var AppHistory = history.useBasename(history.createHistory)({
+    basename: ''
+});
+
 var NewSlide = React.createClass({
 	  render: function() {
 
-	      return <div className={'slide-container ' + this.props.data.cls}>
-				  <div style={{backgroundImage: 'url(' + this.props.data.path + ')'}}
-					className="slide"></div>
-			  </div>;
+	      return <Link to={(this.props.state).toString()}>
+      				<div className={'slide-container ' + this.props.data.cls}>
+      					<div style={{backgroundImage: 'url(' + this.props.data.path + ')'}}
+      						className="slide"></div>
+					</div>
+				</Link>;
 	  }
 });
 
 var SnapViewer = React.createClass({
-	getInitialState: function () {
-		lastSnap = this.props.snData.length - 1;
+	mixins: [ History ],
+	getStateFromStore: function (props) {
+		var currentIdx = props ? +props.params.id - 1 : 0;
+
+		lastSnap = snapList.length - 1;
 
 		return {
+			snData: snapList,
 			action: 'Play',
 			animationProgress: false,
-			currentIdx: 0,
-			nextIdx: 1,
-			prevIdx: lastSnap
-		};
+			currentIdx: currentIdx,
+			nextIdx: (currentIdx === lastSnap ? 0 : currentIdx + 1),
+			prevIdx: (currentIdx === 0 ? lastSnap : currentIdx - 1)
+		}
+	},
+	getInitialState: function () {
+		return this.getStateFromStore();
+    },
+    componentWillReceiveProps(nextProps) {
+        //this.setState(this.getStateFromStore(nextProps));
     },
     prev: function (event) {
     	this.fnslideShow(true, true);
@@ -92,9 +115,9 @@ var SnapViewer = React.createClass({
 		
 		if (isPrev) {
 			setTimeout(function () {
-				this.props.snData[this.state.prevIdx].cls = 'animate shown';
-				this.props.snData[this.state.currentIdx].cls = 'next';
-				this.props.snData[this.state.nextIdx].cls = '';
+				this.state.snData[this.state.prevIdx].cls = 'animate shown';
+				this.state.snData[this.state.currentIdx].cls = 'next';
+				this.state.snData[this.state.nextIdx].cls = '';
 				this.setState({nextIdx: this.state.currentIdx});
 				this.setState({currentIdx: this.state.prevIdx});
 				this.setState({prevIdx: (this.state.currentIdx === 0 ?
@@ -102,21 +125,22 @@ var SnapViewer = React.createClass({
 			}.bind(this), 1);
 		} else {
 			setTimeout(function () {
-				this.props.snData[this.state.prevIdx].cls = '';
-				this.props.snData[this.state.currentIdx].cls = 'animate shown left';
+				this.state.snData[this.state.prevIdx].cls = '';
+				this.state.snData[this.state.currentIdx].cls = 'animate shown left';
 				this.setState({prevIdx: this.state.currentIdx});
 				this.setState({currentIdx: this.state.nextIdx});
 				this.setState({nextIdx: (this.state.currentIdx === lastSnap ?
-						lastSnap: this.state.nextIdx + 1)});
+						0 : this.state.nextIdx + 1)});
 			}.bind(this), 1);
 		}
 
 		// on animation complete
 		setTimeout(function () {
-			this.props.snData[this.state.prevIdx].cls = 'left';
-			this.props.snData[this.state.currentIdx].cls = 'shown';
-			this.props.snData[this.state.nextIdx].cls = 'next';
+			this.state.snData[this.state.prevIdx].cls = 'left';
+			this.state.snData[this.state.currentIdx].cls = 'shown';
+			this.state.snData[this.state.nextIdx].cls = 'next';
 			this.setState({animationProgress: false});
+			this.history.pushState(null, (this.state.currentIdx + 1).toString());
 		}.bind(this), 1500);
 	},
 	render: function() {
@@ -128,14 +152,18 @@ var SnapViewer = React.createClass({
 						</button>
 						<button onClick={this.next}>&gt;</button>
 					</div>
-			   		{this.props.snData.map(function(item) {
-			   			return <NewSlide key={arguments[1]} data={item} />;
+			   		{this.state.snData.map(function(item) {
+			   			return <NewSlide state={arguments[1] + 1} key={arguments[1]} data={item}/>;
 			   		})}
 			  </div>;
 	}
 });
 
 ReactDOM.render(
-	<SnapViewer snData={snapList} />,
-    document.getElementById('snapContainer')
+	<Router history={AppHistory}>
+	    <Route path='/' component={SnapViewer}>
+	    	<Route path=":id" component={NewSlide} />
+	    </Route>
+	</Router>,
+	document.getElementById('snapContainer')
 );
