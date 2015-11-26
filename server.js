c1ba58dd94c62f2848b3856f673c3b9fa5a03913
sree-bh <sritama.imps@gmail.com>
2015-11-26 11:17:49 +0530

    updated server.js

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8085);
+//var connect = require('connect');
+//var serveStatic = require('serve-static');
+//connect().use(serveStatic(__dirname)).listen(8085);
+
+
+var express = require('express');
+var path = require('path');
+var port = process.env.PORT || 8085;
+var app = express();
+
+// serve static assets normally
+app.use(express.static(__dirname));
+
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, 'react-snap', 'index.html'))
+});
+
+app.listen(port);
+console.log("server started on port " + port);
